cf30436b216a2713a5cca18de4f11ae94d83c82f
anuj-af <anujkhandelwal.0404@gmail.com>
2024-06-16 20:30:58 +0530

    added styles

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ app.set('views', path.join(__dirname,'views'));
 
 app.use(express.urlencoded({extended: true}));
 app.use(methodOverride('_method'));
+app.use(express.static(path.join(__dirname,'public')));
 
 
 const Community = require('./models/community');
