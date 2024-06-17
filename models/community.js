44cbabfc5fd30d2f0b51762da2c14554b562a3db
anuj-af <anujkhandelwal.0404@gmail.com>
2024-06-17 18:05:32 +0530

    modified styles

--- models/community.js
+++ models/community.js
@@ -8,6 +8,10 @@ const communitySchema = new mongoose.Schema({
     description: {
         type: String,
         default: "No Description Available for this community."
+    },
+    profileImg: {
+        type: String,
+        default: "https://img.freepik.com/free-photo/3d-portrait-people_23-2150794029.jpg?t=st=1718601772~exp=1718605372~hmac=e7f53db83aa6b891c9ceeb849a8a6866465aa530d92a8d2533ae9a3da82cef59&w=1060"
     }
 })
 
